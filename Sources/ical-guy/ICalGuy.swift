5141d1efc09158d3b4c0e77f4dd62d98670ded83
Josh Priddle <jpriddle@me.com>
2026-02-15 15:08:57 -0500

    Bump version to v0.3.1

--- Sources/ical-guy/ICalGuy.swift
+++ Sources/ical-guy/ICalGuy.swift
@@ -5,7 +5,7 @@ struct ICalGuy: AsyncParsableCommand {
   static let configuration = CommandConfiguration(
     commandName: "ical-guy",
     abstract: "Query macOS calendar events and output as JSON or text.",
-    version: "0.3.0",
+    version: "0.3.1",
     subcommands: [EventsCommand.self, CalendarsCommand.self, MeetingCommand.self],
     defaultSubcommand: EventsCommand.self
   )
